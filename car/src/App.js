8bdd49ca87324e06d853baf1db2a0b36257075df
Sakshi Rai <97029528+Saarah123@users.noreply.github.com>
2023-01-16 16:34:55 +0530

    BMW 3D Model

---
+++ car/src/App.js
@@ -0,0 +1,23 @@
+import {Canvas} from "@react-three/fiber";
+import {useGLTF , Stage , PresentationControls } from "@react-three/drei";
+
+function Model(props){
+  const {scene} = useGLTF("/bmw.glb");
+  return <primitive object = {scene}  {...props}/>
+}
+
+function App() {
+  return (
+   <Canvas dpr={[1,2]} shadows camera={{fov:45}} style={{"position":"relative"}}>
+    <color attach="background" args={["#101010"]}></color>
+    <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI/4]}>
+      <Stage environment={"sunset"}>
+        <Model scale={0.03}/>
+      </Stage>
+
+    </PresentationControls>
+   </Canvas>
+  );
+}
+
+export default App;
